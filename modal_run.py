32c31538fb42a61c3c98865148f94bc7f9442c5f
tlebryk <tlebryk02@gmail.com>
2025-05-12 14:43:15 -0400

    modal

--- modal_run.py
+++ modal_run.py
@@ -45,3 +45,67 @@ def train():
         ],
         check=True,
     )
+
+
+# Point to the folder that contains your Dockerfile
+image2 = (
+    modal.Image.debian_slim(python_version="3.10")
+    .pip_install(
+        "beautifulsoup4==4.11.1",
+        "einops==0.6.0",
+        "evaluate==0.4.0",
+        "protobuf==3.20.3",
+        "safetensors==0.3.1",
+        "scikit-learn==1.2.2",
+        "scipy==1.10.1",
+        "thinc==8.1.5",
+        "threadpoolctl==3.1.0",
+        "tqdm==4.64.1",
+        "wandb==0.13.4",
+        "rouge_score",
+        "accelerate==0.20.3",
+        "fairscale==0.4.12",
+        "timm==0.6.12",
+        "bert-score==0.3.13",
+        "tokenizers==0.13.1",
+        "transformers==4.30.2",
+        "gdown",
+        "pycocotools",
+        "pycocoevalcap",
+    )
+    .add_local_dir("./", remote_path="./")
+    # .add_local_dir("configs", remote_path="/root/configs")
+    # .add_local_file("train.json", remote_path="/root/train.json")
+    # .add_local_file("val.json", remote_path="/root/val.json")
+    # .add_local_file("test.json", remote_path="/root/test.json")
+    # .add_local_python_source("llada", "llada_train", "eval_helper")
+)
+
+
+@app.function(
+    image=image2,
+    secrets=[modal.Secret.from_name("wandb-secret")],  # <-- injects env var
+    gpu="L4",
+    timeout=60 * 60 * 6,
+)
+def train2():
+    subprocess.run(
+        [
+            "accelerate",
+            "launch",
+            "main.py",
+            "--epoch",
+            "50",
+            "--notes",
+            "50_w_pretrain",
+            # "--bsz",
+            # "4",
+            # "--seqlen",
+            # "150",
+            # "--mixed_precision",
+            # "fp16",
+            "--full_model_path",
+            "pytorch_model.bin",
+        ],
+        check=True,
+    )
